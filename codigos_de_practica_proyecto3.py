7f4fd92c088b69ddde32be35daeab31df971a365
ignacio1620 <matias.canales.8b@gmail.com>
2024-11-22 16:33:56 -0300

    Update codigos_de_practica_proyecto3.py

--- codigos_de_practica_proyecto3.py
+++ codigos_de_practica_proyecto3.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 # Definimos la página principal
 def pagina_principal():
-    st.title("Descripción del Proyecto")
+    st.subtitle("Descripción del Proyecto")
     st.write(
         "Este proyecto tiene como objetivo recopilar y analizar información relevante sobre diversos países del mundo, "
         "incluyendo datos sobre su población, idiomas, densidad, área, territorio, etc. A través de una plataforma digital interactiva."
@@ -20,11 +20,33 @@ def visualizacion_datos():
 def graficos_interactivos():
     st.title("Gráficos Interactivos")
     st.write("Esta sección permite interactuar con gráficos sobre diversos parámetros de los países.")
-    # Aquí puedes incluir gráficos interactivos con Plotly, Altair, Matplotlib, etc.
-    # Ejemplo de un gráfico interactivo:
-    # import plotly.express as px
-    # fig = px.bar(data_frame=df, x="country", y="population")
-    # st.plotly_chart(fig)
+ import pandas as pd
+import matplotlib.pyplot as plt
+import requests
+
+url = 'https://restcountries.com/v3.1/all'
+response = requests.get(url)
+countries_data = response.json()
+
+df = pd.DataFrame(countries_data)
+
+conteo_grupos = df.groupby('region')['name'].count()
+
+plt.figure(figsize=(10, 5))
+conteo_grupos.plot(kind='bar')
+
+plt.title('Cantidad de paises por Región') 
+plt.xlabel('Región')  
+plt.ylabel('Cantidad de paises')
+plt.xticks(rotation=45)
+plt.grid(axis='y')
+
+for i, count in enumerate(conteo_grupos):
+    plt.text(i, count + 0.2, str(count), ha='center', va='bottom', fontsize=10)
+
+plt.tight_layout()
+
+plt.show()
 
 # Función principal que gestiona la navegación
 def main():
